100f3eeeec695b13b3796738cea456244dff3de7
u1751855481-maker <u1751855481@gmail.com>
2025-11-24 16:18:54 +0100

    Update ET3_Datos_NombreEquipo.js

--- ET3_Datos_NombreEquipo.js
+++ ET3_Datos_NombreEquipo.js
@@ -1,6 +1,7 @@
 // Estructura requerida para datos del equipo en ET3.
 // Sustituir estos valores dummy por los definitivos antes de la entrega.
 const def_datos_NombreEquipo = [
+    { entrega: "ET3", nombre: "José Manuel Pérez Suárez", dni: "77014609Y", horas: 50},
     { entrega: "ET3", nombre: "Nombre Alumno 1", dni: "00000000A", horas: 10 },
     { entrega: "ET3", nombre: "Nombre Alumno 2", dni: "00000000B", horas: 8 },
     { entrega: "ET3", nombre: "Nombre Alumno 3", dni: "00000000C", horas: 12 }
